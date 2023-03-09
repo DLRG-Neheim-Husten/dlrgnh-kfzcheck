573c48b4aa71a2d2783f723f113d88e3e340acfe
Christian Kaiser <ckaiser79@users.noreply.github.com>
2023-03-09 22:59:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
-file1 = open('kennzeichen.txt', 'r')
+file1 = open('./kennzeichen.txt', 'r')
 Lines = file1.readlines()
 file1.close()
-s = set(())
+app.data = set(())
 
 for line in Lines:
-    s.add(line)
+    app.data.add(line)
 
 @app.route("/")
 def hello_world():
     kennzeichen = request.args.get('kennzeichen')
-    return kennzeichen is not None and kennzeichen in s
+    return kennzeichen is not None and kennzeichen in app.data
