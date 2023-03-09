6cc2a74dbe34226d6084df4be55c324f5a27960a
Christian Kaiser <ckaiser79@users.noreply.github.com>
2023-03-09 22:14:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+file1 = open('myfile.txt', 'r')
+Lines = file1.readlines()
+file1.close()
+s = set(())
+
+for line in Lines:
+    s.add(line)
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    kennzeichen = request.args.get('kennzeichen')
+    return kennzeichen in s
