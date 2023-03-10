155104a85e7efa1191b565b25b6529a4c9d9accc
Christian Kaiser <christiankaiser@mailbox.org>
2023-03-10 18:10:55 +0100

    cors

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
-from flask import Response
 from flask import jsonify
+from flask import Response
 from flask import request
 from flask_restx import Resource, Api
 
 import logging
 import re
- 
+
+CORS_CHECK_ENABLED = True
+
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
@@ -32,36 +34,77 @@ for key, value in app.data.items():
     logging.info(key + ": " + value)
 
 
+allowedCorsDomains = set(())
+allowedCorsDomains.add('https://neheim.dlrg.de')
+allowedCorsDomains.add('https://localhost')
+allowedCorsDomains.add('http://localhost')
+
+class AccessForbiddenException(Exception):
+    pass
+
 @api.route("/query/<string:kennzeichen>")
 @api.doc(params={'kennzeichen': '''
  Kennzeichen, nachdem gesucht wird. Case insensitive. Nicht alphanumerische
  Zeichen werden bei der Suche entfernt
 '''}, )
 class Kennzeichen(Resource):
+
     @api.doc(
         responses={
             200: 'Kennzeichen wurde gefunden. Body enthält die genau gespeicherten Kennzeichen.',
             204: 'Kennzeichen wurde nicht gefunden.',
+        },
+        headers= {
+            'Origin': 'Origin set by browser to determine if CORS request is allowed.'
         })
     def get(self, kennzeichen):
-        queryKennzeichen = kennzeichen.upper()
-        queryKennzeichen = re.sub(r'\W+', "", queryKennzeichen)
-        
+
+        try:
+            headerOrigin = self.loadValidatedOriginForCorsRequest()
+        except AccessForbiddenException as err:  
+            return Response(err, 403)
+
+        resp = self.createResponseFromData(kennzeichen)
+        if headerOrigin is not None:
+            resp.headers['Access-Control-Allow-Origin'] = headerOrigin
+
+        return resp
+    
+    def createResponseFromData(self, kennzeichen):
+        kennzeichenKey = kennzeichen.upper()
+        kennzeichenKey = re.sub(r'\W+', "", kennzeichenKey)
+
         registeredKennzeichen = None
         status = 204
         result = dict()
         
-        logging.debug("Query for '%s'" % queryKennzeichen)
-        if queryKennzeichen in app.data:            
-            registeredKennzeichen = app.data[queryKennzeichen]
+        logging.debug("Query for '%s'" % kennzeichenKey)
+        if kennzeichenKey in app.data:            
+            registeredKennzeichen = app.data[kennzeichenKey]
             result['kennzeichen'] = registeredKennzeichen
 
             status = 200
         
         logging.debug("Status %s with %s" % (status, result))
-        return result, status
-        #return Response(result, status=status, mimetype='application/json')
+        resp = jsonify(result)
+        resp.status = status
+        return resp
+
         
+    def loadValidatedOriginForCorsRequest(self):
+        if not CORS_CHECK_ENABLED:
+            return None
+
+        headers = request.headers
+        
+        if 'Origin' not in headers:
+            raise AccessForbiddenException("Missing 'Origin' header in request")
+
+        headerOrigin = request.headers['Origin']
+        if headerOrigin not in allowedCorsDomains:
+            raise AccessForbiddenException("'Origin' not in %s" % str(allowedCorsDomains))
+
+        return headerOrigin
 
 if __name__ == '__main__':
     app.run(debug=False)
